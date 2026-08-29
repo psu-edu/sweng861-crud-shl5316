6d742a522c5e14fb12fd2b0cfaa6f16e716ecdab
Samuel Lee <shl5316@psu.edu>
2026-08-29 14:17:28 -0400

    feat: Introduce CORS configuration

--- backend/src/main/java/com/sweng861/agiletracker/AgileTrackerApplication.java
+++ backend/src/main/java/com/sweng861/agiletracker/AgileTrackerApplication.java
@@ -18,7 +18,9 @@ public class AgileTrackerApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:8000").allowedMethods("GET");
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000", "http://localhost:8000")
+						.allowedMethods("GET");
 			}
 		};
 	}
