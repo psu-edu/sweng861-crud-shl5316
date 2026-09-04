4a3b8f3c9acbbef71f2fbb3ab162da7c4d72690b
Samuel Lee <shl5316@psu.edu>
2026-09-04 14:25:11 -0400

    chore: remove CrossOrigin from HealthController

--- backend/src/main/java/com/sweng861/agiletracker/HealthController.java
+++ backend/src/main/java/com/sweng861/agiletracker/HealthController.java
@@ -1,12 +1,10 @@
 package com.sweng861.agiletracker;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8000"})
 @RestController
 public class HealthController {
 
